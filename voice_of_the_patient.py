9020e7ea17e0de86e5ece3f6158eb369690794a0
Prerna <154355981+Prerna77Arora@users.noreply.github.com>
2025-10-01 22:44:24 +0530

    Update voice_of_the_patient.py

--- voice_of_the_patient.py
+++ voice_of_the_patient.py
@@ -1,8 +1,14 @@
-#setup audio recoder(ffmpeg and portaudio)
+# if you dont use pipenv uncomment the following:
+# from dotenv import load_dotenv
+# load_dotenv()
+
+#Step1: Setup Audio recorder (ffmpeg & portaudio)
+# ffmpeg, portaudio, pyaudio
 import logging
-import speech_recognition as sr 
+import speech_recognition as sr
 from pydub import AudioSegment
 from io import BytesIO
+
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def record_audio(file_path, timeout=20, phrase_time_limit=None):
@@ -36,15 +42,15 @@ def record_audio(file_path, timeout=20, phrase_time_limit=None):
     except Exception as e:
         logging.error(f"An error occurred: {e}")
 
-audio_filepath=("patient_voice_test_for_patient.mp3")
-record_audio(file_path=audio_filepath)
+audio_filepath="patient_voice_test_for_patient.mp3"
+#record_audio(file_path=audio_filepath)
 
-
-#setup speech to text-STT-Model for transcription
-import os 
+#Step2: Setup Speech to text–STT–model for transcription
+import os
 from groq import Groq
+
 GROQ_API_KEY=os.environ.get("GROQ_API_KEY")
-stt_model=("whisper-large-v3")
+stt_model="whisper-large-v3"
 
 def transcribe_with_groq(stt_model, audio_filepath, GROQ_API_KEY):
     client=Groq(api_key=GROQ_API_KEY)
